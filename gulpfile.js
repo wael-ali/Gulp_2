30629373194886f100c4f865a1696273545e36c0
Weal Ali <wealali595@gmail.com>
2020-06-04 18:24:52 +0200

    add scssTask to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,25 @@
   const uglify        = require("gulp-uglify");
 // File path variables
 const pathes = {
-  scss: 'src/scss/**/*.scss',
-  js: 'src/js/**/*.js',
+  src: {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+  },
+  dest: {
+    css: 'dist/css/main.css',
+    js: 'dist/js/main.js',
+  }
 }
 // Sass task
+function scssTask() {
+  return src(pathes.src.scss) // get all scss files
+    .pipe(sourcemaps.init()) // should be run before the sass function
+    .pipe(sass()) // compile the scss files into css
+    .pipe(postcss([autoprefixer(), cssnano()])) // put prefixes + minify css
+    .pipe(sourcemaps.write('.')) // finish maping css and write the map in same directory
+    .pipe(dest(pathes.dest.css))
+  ;
+}
 
 // JS task
 
