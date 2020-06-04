f01bd8b1f8aa7ae1d8c28e6fb6fcee164a4cfefe
Weal Ali <wealali595@gmail.com>
2020-06-04 18:08:10 +0200

    initialize modules in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 // Initialize modules
-
+  const { src, dest, watch, series, parallel } = require("gulp");
+  const autoprefixer  = require("autoprefixer");
+  const cssnano       = require("cssnano");
+  const concat        = require("gulp-concat");
+  const postcss       = require("gulp-postcss");
+  const replace       = require("gulp-replace");
+  const sass          = require("gulp-sass");
+  const sourcemaps    = require("gulp-sourcemaps");
+  const uglify        = require("gulp-uglify");
 // File path variables
 
 // Sass task
